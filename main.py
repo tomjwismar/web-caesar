9e5af3a380833e4f844a884b25a3b6f8e7f2520c
Tom Wismar <tomjwismar@gmail.com>
2017-03-27 01:53:06 -0500

    added html escape

--- main.py
+++ main.py
@@ -16,6 +16,7 @@
 #
 import webapp2
 import caesar
+import cgi
 
 def build_page(textarea_content):
     rot_label = "<label> Rotate by:</label>"
@@ -42,7 +43,8 @@ class MainHandler(webapp2.RequestHandler):
         message = self.request.get("message")
         rotation = int(self.request.get("rotation"))
         encrypted_message = caesar.encrypt(message, rotation)
-        content = build_page(encrypted_message)
+        escaped_message = cgi.escape(encrypted_message)
+        content = build_page(escaped_message)
         self.response.write(content)
 
 
